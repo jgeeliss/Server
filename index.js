b1383c4f2f613b2ec68a928b99c8b09e701ce1ab
Jesse Geelissen <jgeeliss@gmail.com>
2020-06-28 09:52:02 +0200

    use modules

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 
 
-var http = require("http")
-
-function onRequest(request,response) {
-    response.writeHead(200, {"content-Type": "text/plain"})
-    response.write("Hello World")
-    response.end()
-}
-
-http.createServer(onRequest).listen(8888)
-
-
-/*var server = require ("./server");
+var server = require ("./server");
 var router = require ("./router");
 var requestHandlers = require ("./requestHandlers");
 
@@ -20,4 +9,4 @@ handlesList["/"] = requestHandlers.start;
 handlesList["/start"] = requestHandlers.start;
 handlesList["/upload"] = requestHandlers.upload;
 
-server.startupserver(router.runrouter, handlesList);*/
+server.startupserver(router.runrouter, handlesList);
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 var http = require("http");
 var url = require("url");
 
-function start(route, handleList){
-	
-function onRequest(request, response) {
-	var pathname = url.parse(request.url).pathname;
-	console.log("request for " + pathname + " received");
-	route(handleList, pathname, response);
-	
-	//response.writeHead(200, {"Content-Type": "text/plain"});
-	//response.write("test reply server zondag");
-	//response.end();
-};
-console.log("server has started");
+function start(route, handleList) {
 
-http.createServer(onRequest).listen(8888);
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("request for " + pathname + " received");
+		route(handleList, pathname, response);
+
+		//response.writeHead(200, {"Content-Type": "text/plain"});
+		//response.write("test reply server zondag");
+		//response.end();
+	};
+	console.log("server has started");
+
+	http.createServer(onRequest).listen(8888);
 
 }
 
